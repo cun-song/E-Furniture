9711f8baff98c3b6bbaa39fd59374bbad642aa6a
Rizky Kurniawan <115348639+rzkyKrniawan@users.noreply.github.com>
2022-12-13 16:19:16 +0700

    Add files via upload

---
+++ resources/views/home_page.blade.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>E-Furniture</title>
+        <style>
+            .allText {
+                font-family: arial;
+            }
+            .background_E-Furniture {
+                background-image: url("/photo/spacejoy-9M66C_w_ToM-unsplash.jpg");
+                padding-top: 190px;
+                padding-bottom: 190px;
+                background-size: cover;
+            }
+            .E-Furniture {
+                font-size: 50px;
+                text-align: center;
+                color: white;
+                font-weight: 400px;
+            }
+            .subText {
+                text-align: center;
+                color: white;
+            }
+            .subTitle {
+                text-align: center;
+            }
+            .subText_subTitle {
+                text-align: center;
+            }
+            .secondSection {
+                padding-top: 110px;
+                padding-bottom: 110px;
+            }
+            .subTitle {
+                
+            }
+            .subText_subTitle {
+                color: gray;
+            }
+            .firstPhoto {
+                width: 290px;
+                height: 320px;
+                margin-right: 20px;
+                object-fit: cover;
+            }
+            .secondPhoto {
+                width: 290px;
+                height: 320px;
+                margin-right: 20px;
+                object-fit: cover;
+            }
+            .thirdPhoto {
+                width: 290px;
+                height: 320px;
+                margin-right: 20px;
+                object-fit: cover;
+            }
+            .photo {
+                padding: 25px;
+                padding-left: 200px;
+            }
+            .firstPosition {
+                padding-top: 30px;
+                display: inline-block;
+            }
+            .secondPosition {
+                display: inline-block;
+            }
+            .thirdPosition {
+                padding-top: 30px;
+                display: inline-block;
+            }
+            .thirdSection {
+                background-color: #e2f9f7;
+                padding-top: 110px;
+                padding-bottom: 110px;
+            }
+            .thirdSection_Left {
+                display: inline-block;
+                width: 620px;
+                text-align: center;
+
+            }
+            .thirdSection_Right {
+                display: inline-block;
+                width: 620px;
+                vertical-align: middle;
+            }
+            .photoSection3 {
+                height: 450px;
+                border-radius: 10px;
+                object-fit: cover;
+            }
+            .findButton {
+                padding: 14px;
+                padding-left: 38px;
+                padding-right: 38px;
+                border-radius: 17px;
+                background-color: #03344F;
+                border: none;
+                color: white;
+                cursor: pointer;
+            }
+            .fourSection {
+                background-color: #03344F;
+                padding-top: 90px;
+                padding-bottom: 90px;
+                color: white;
+            }
+            .photoFour {
+                background-color: red;
+                width: 185px;
+                height: 185px;
+                margin-right: 18px;
+                border-radius: 10px;
+                border: none;
+                object-fit: cover;
+            }
+            .positionPhotoFour {
+                display: inline-block;
+            }
+            .fourSection_Left {
+                display: inline-block;
+                width: 380px;
+                vertical-align: middle;
+                padding-left: 90px;
+
+            }
+            .fourSection_Right {
+                display: inline-block;
+                vertical-align: middle;
+            }
+            .footerSection {
+                padding-top: 25px;
+                padding-bottom: 25px;
+                background-color: #011a28;
+                color: white;
+                text-align: center;
+            }
+            .listSubMenu {
+                list-style: none;
+            }
+            .list_menu {
+                text-decoration: none;
+                margin-right: 75px;
+                color: black;
+            }
+            .list {
+                margin-top: -20px;
+                margin-left: 800px;
+                display: flex;
+            }
+            .logoNavigator {
+                font-size: 25px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="allText">
+            <div>
+                <nav>
+                    <ul class="listSubMenu">
+                        <div>
+                            <h4 class="logoNavigator">
+                                E-Furniture
+                            </h4>  
+                        </div>
+                        <div class="list">
+                            <li>
+                                <a class="list_menu" href="">Home</a>
+                            </li>
+                            <li>
+                                <a class="list_menu" href="">Products</a>
+                            </li>
+                            <li>
+                                <a class="list_menu" href="">Editor</a>
+                            </li>
+                            <li>
+                                <a class="list_menu" href="">Login</a>
+                            </li>
+                        <div>
+                    </ul>
+                </nav>
+            </div>
+            <div>
+                <div class="background_E-Furniture">    
+                    <h1 class="E-Furniture">
+                        E-Furniture
+                    </h1>
+                    <p class="subText">
+                        E-Furniture adalah website yang memudahkan Anda dalam
+                        mencari furniture yang cocok
+                    </p>
+                </div>
+            </div>
+            <div class="secondSection">
+                <div>
+                    <div>
+                        <h2 class="subTitle">
+                            Inspiration Collection
+                        </h2>
+                    </div>
+                    <div class="subText_subTitle">
+                        <p>
+                            Kumpulan furniture yang lengkap dapat memberi Anda inspirasi
+                        </p>
+                    </div>
+                </div>
+                <div class="photo">
+                    <div class="firstPosition">
+                        <div>
+                            <img src="/photo/spacejoy-c0JoR_-2x3E-unsplash.jpg" class="firstPhoto">
+                        </div>
+                    </div>
+                    <div class="secondPosition">
+                        <div>
+                            <img src="/photo/spacejoy-IH7wPsjwomc-unsplash.jpg" class="secondPhoto">
+                        </div>
+                    </div>
+                    <div class="thirdPosition">
+                        <div>
+                            <img src="/photo/nathan-fertig-FBXuXp57eM0-unsplash.jpg" class="thirdPhoto">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="thirdSection">
+                <div class="thirdSection_Left">
+                    <div>
+                        <div>
+                        <h2 class="textSection3">
+                            Get Your Furniture
+                        </h2>
+                    </div>
+                    <div>
+                        <p>
+                            Description
+                        <p>
+                    </div>
+                    <div>
+                        <button class="findButton">
+                            Find
+                        </button>
+                    </div>
+                    </div>
+                </div>
+                <div class="thirdSection_Right">
+                    <div>
+                        <img src="/photo/collov-home-design-HmHArS-HvNw-unsplash.jpg" class="photoSection3">
+                    </div>
+                </div>
+            </div>
+            <div class="fourSection">
+                <div class="fourSection_Left">
+                    <h4>
+                        Nama
+                    </h4>
+                    <p>
+                        Do eiusmod tempor incididunt ut labore et 
+                    </p>
+                    <h4>
+                        Follow Us
+                    </h4>
+                </div>
+                <div class="fourSection_Right">
+                    <div>
+                        <h4>
+                            Upcoming Product
+                        </h4>
+                    </div>
+                    <div class="thosePhoto">
+                        <div class="positionPhotoFour">
+                            <img src="/photo/spacejoy-vPKNB_gc23Q-unsplash.jpg" class="photoFour">
+                        </div>
+                        <div class="positionPhotoFour">
+                            <img src="/photo/spacejoy-YnLJ3rM4VtI-unsplash.jpg" class="photoFour">
+                        </div>
+                        <div class="positionPhotoFour">
+                            <img src="/photo/kara-eads-buhmhprfo3g-unsplash.jpg" class="photoFour">
+                        </div>
+                        <div class="positionPhotoFour">
+                            <img src="/photo/aranprime-KbytCpI1i5I-unsplash.jpg" class="photoFour">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footerSection">
+                    Nama anggota
+            </div>
+        </div>
+    </body>
+</html>
