b50b6ee6affa9b00625b77abd971eedad2a54099
malikfajarh <106681713+malikfajarh@users.noreply.github.com>
2022-12-16 09:57:56 +0700

    nambah image dan tombol qty serta improve css

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,72 +1,33 @@
+@php
+    use Illuminate\Support\Facades\Storage;
+@endphp
+
 @extends('partial.index')
 @section('container')
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<script src="../E-Furniture/resources/js/qty-amount.js"></script>
-
 <script>
-    $(document).ready(function(){
-
-var quantitiy=0;
-   $('.quantity-right-plus').click(function(e){
-
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        var quantity = parseInt($('#quantity').val());
-
-        // If is not undefined
-
-            $('#quantity').val(quantity + 1);
-
-
-            // Increment
-
-    });
-
-     $('.quantity-left-minus').click(function(e){
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        var quantity = parseInt($('#quantity').val());
-
-        // If is not undefined
-
-            // Increment
-            if(quantity>0){
-            $('#quantity').val(quantity - 1);
-            }
-    });
-
-});
-
+    function increment() {
+        document.getElementById('qty-amount').stepUp();
+    }
+    function decrement() {
+        document.getElementById('qty-amount').stepDown();
+    }
 </script>
 
 <style>
     .left-half{
-    /* background: url(bg-1.jpg); */
-    width: 50%;
-    position: absolute;
-    left: 0px;
-    height: 100%;
-    }
-
-    #text{
-        color: black;
-        font-size: 35px;
-        text-align: center;
-        margin-top: 45%;
-
+        width: 50%;
+        position: absolute;
+        left: 0px;
+        height: 100%;
     }
 
     .right-half{
-    width: 50%;
-    position: absolute;
-    background-color: #DBE8FC;
-    right: 0px;
-    height: 100%;
+        width: 50%;
+        position: absolute;
+        background-color: #DBE8FC;
+        right: 0px;
+        height: 100%;
     }
 
     .content{
@@ -75,7 +36,7 @@ var quantitiy=0;
         margin-top: 45px;
         margin-left: 30px;
         margin-right: 30px;
-        color: black;
+        color: #2B495E;
     }
 
     #nama-produk{
@@ -87,18 +48,15 @@ var quantitiy=0;
     #deskripsi{
         margin-top: 10px;
         font-size: 20px;
+        color: black;
     }
 
     #qty{
         margin-top: 10px;
     }
 
-    .qty-amount{
-        margin-top: 46%;
-    }
-
-    #total-price{
-        margin-top: 10px;
+    .end{
+        margin-top: 8px;
     }
 
     #check-out{
@@ -116,18 +74,63 @@ var quantitiy=0;
         color: rgb(255, 255, 255);rgb(0, 27, 41)
     }
 
+    /* #decrement{
+        margin-left; 100px;
+        color: green;
+    } */
+
     #qty-amount{
-        background-color: #DBE8FC;
-        margin-left: 10px;
-        width: 70px;
+        background-color: #054C73;
+        /* margin-left: 10px; */
+        width: 60px;
         border: none;
+        margin-top: 46%;
+        color: white;
+        text-align: center;
+        padding: 0px 6x;
+        /* border: 2px solid white; */
+        border-radius: 3px;
+    }
+
+    .amount-button{
+        background-color: #054C73;
+        border: none;
+        color: white;
+        padding: 1px 8px;
+        border-radius: 3px;
+
+    }
+
+    .amount-button:hover{
+        background-color: #0073b1;
+        color: rgb(255, 255, 255);rgb(0, 27, 41)
+    }
+
+    .img{
+        position: relative;
+        height: 100%;
+    }
+
+    #price{
+        position: absolute;
+        bottom: 28px;
+        right: 36px;
+        color: white;
+        font-family: Cambria;
+        background-color: #2B495E;
+        border-radius: 50%;
+        padding: 20px;
+        font-weight: bold;
+        font-size: 40px;
+        box-shadow: 0px 0px 3px black;
     }
 
 </style>
 
 <body>
     <div class="left-half">
-        <div id="text">Gambar produk</div>
+        <img src="{{storage::url("images/lamp.jpg")}}" alt="" class="img">
+        <span id="price">$75</span>
     </div>
 
     <div class="right-half">
@@ -136,36 +139,19 @@ var quantitiy=0;
             <div id="deskripsi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit architecto fugit illum, sint animi iure, quia non totam, ea velit tenetur quod nobis rerum! Cumque facere sapiente quod aliquid dolorum!</div>
             <div id="qty">Stock: </div>
 
-            <div class="qty-amount">
+            <div class="qty-amounts">
                 <span id="qty2">Quantity: </span>
-                <input type="number" id="qty-amount" value="0">
-                {{-- <div class="col-lg-2">
-                    <div class="input-group">
-                        <span class="input-group-btn">
-                            <button type="button" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
-                            <span class="glyphicon glyphicon-minus"></span>
-                            </button>
-                        </span>
-                        <input type="text" id="quantity" name="quantity" class="form-control input-number" value="10" min="1" max="100">
-                        <span class="input-group-btn">
-                            <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
-                                <span class="glyphicon glyphicon-plus"></span>
-                            </button>
-                        </span>
-
-                    </div>
-                </div>
-            </div> --}}
+                <button onclick="decrement()" class="amount-button" id="decrement">-</button>
+                <input type="number" id="qty-amount" value="0" min="0">
+                <button onclick="increment()" class="amount-button" >+</button>
+            </div>
 
             <div class="end">
                 <span id="total-price">Total Price: $</span>
                 <a href="/" id="check-out">Check out</a>
             </div>
-
-        </div>
     </div>
 
 
 </body>
 @endsection
-
